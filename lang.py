a70c93b052f52351fc238ecfd5e460315a5bc53b
Hilal Alantari <hilalkhaledhilal2020@gmail.com>
2025-09-13 06:39:36 +0300

    0

---
+++ lang.py
@@ -0,0 +1,12 @@
+strings = {
+    "en": {
+        "title": "Cat vs Dog Classifier",
+        "upload": "Upload Image",
+        "result": "Prediction: "
+    },
+    "ar": {
+        "title": "مُصنف قطط وكلاب",
+        "upload": "تحميل صورة",
+        "result": "النتيجة: "
+    }
+}
